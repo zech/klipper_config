39ada6fcb8219d7e2c8871cbeed1987449d28028
Tim Zech <tim.zech@gmail.com>
2022-06-10 00:13:09 +0200

    Autocommit from 2022-06-10 00:13:09
    Klipper on commit: f42ce3e
    
    Moonraker on commit: 5081321
    
    Mainsail version: v2.2.0-beta5

--- scripts/splashscreen.sh
+++ scripts/splashscreen.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/fbi -t 5 -d /dev/fb0 --noverbose /home/tzech/klipper_config/boot/boot-image.png
+/usr/bin/fbi -d /dev/fb0 --noverbose /home/tzech/klipper_config/boot/boot-image.png
